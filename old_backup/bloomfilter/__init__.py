a68a333611e6a5c6817b1665da1d0151efe80dd2
Marvin <wdcpop@gmail.com>
2018-03-07 18:16:08 -0800

    delete test files

--- old_backup/bloomfilter/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-#coding:utf8
-"""
-作者:刘洋
-邮箱:liuyang@wallscreetcn.com
-微信:475090118
-时间:16-5-8
-"""
-from bloomfilter import BloomFilter
--- old_backup/settings.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# coding:utf8
-
-from proxies import proxy_list as PROXIES
-FOREIGN_PROXIE='socks5://127.0.0.1:49999'
